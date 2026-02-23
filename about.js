19c65e7598c82348197b02ebdb69882dcd64be01
Drazil5678 <102932928+Drazil5678@users.noreply.github.com>
2026-02-23 17:12:21 -0500

    Update about.js

--- about.js
+++ about.js
@@ -1,23 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  console.log("about.js is connected");
+
   const images = document.querySelectorAll(".arc-gallery img");
   const modal = document.getElementById("modal");
   const modalImg = document.getElementById("modal-img");
   const closeBtn = document.querySelector(".close");
-console.log("about.js is connected");
+
+  // When gallery image is clicked
   images.forEach(img => {
     img.addEventListener("click", function () {
       modal.style.display = "flex";
       modalImg.src = this.src;
     });
+  });
 
+  // Close button click
   closeBtn.addEventListener("click", function () {
     modal.style.display = "none";
   });
 
+  // Click outside image closes modal
   modal.addEventListener("click", function (e) {
     if (e.target === modal) {
       modal.style.display = "none";
     }
   });
 
+});
