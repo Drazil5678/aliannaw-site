d0a6c593e5ca75d9dd6a8ca054208308da1936a2
Drazil5678 <102932928+Drazil5678@users.noreply.github.com>
2026-02-23 16:35:33 -0500

    Update about.js

--- about.js
+++ about.js
@@ -1,18 +1,25 @@
-<script>
-const modal = document.getElementById("modal");
-const modalImg = document.getElementById("modal-img");
-const images = document.querySelectorAll(".arc-gallery img");
-const closeBtn = document.getElementById("close");
+document.addEventListener("DOMContentLoaded", function () {
 
-images.forEach(img => {
-  img.addEventListener("click", () => {
-    modal.style.display = "flex";
-    modalImg.src = img.src;
+  const images = document.querySelectorAll(".arc-gallery img");
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const closeBtn = document.querySelector(".close");
+
+  images.forEach(img => {
+    img.addEventListener("click", function () {
+      modal.style.display = "flex";
+      modalImg.src = this.src;
+    });
   });
-});
 
-closeBtn.onclick = () => modal.style.display = "none";
-modal.onclick = e => {
-  if (e.target === modal) modal.style.display = "none";
-};
-</script>
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  modal.addEventListener("click", function (e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+});
